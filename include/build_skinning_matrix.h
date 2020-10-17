f12a23abeba603e64d05545bece5e47e12a0ac3a
Abhishek Madan <abhi.madan01@gmail.com>
2020-10-16 23:30:55 -0400

    Fix comment with correct dimensions

--- include/build_skinning_matrix.h
+++ include/build_skinning_matrix.h
@@ -6,6 +6,6 @@
 //  T - the mx4 tetrahedron connectivity matrix. Each row contains to indices into V that indicate a spring between those vertices.
 //  V_skin - lx3 matrix of vertices of the display mesh
 //Output:
-//  N - the lxn sparse skinning matrix 
+//  N - the 3lx3n sparse skinning matrix 
 void build_skinning_matrix(Eigen::SparseMatrixd &N, Eigen::Ref<const Eigen::MatrixXd> V, Eigen::Ref<const Eigen::MatrixXi> T, 
                                                    Eigen::Ref<const Eigen::MatrixXd> V_skin);
